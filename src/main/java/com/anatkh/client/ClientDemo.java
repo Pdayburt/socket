eb8af47d25448e7d7902ab9dc63ade1526a5b1ad
xuchao <pdayburt@gmail.com>
2023-12-04 22:25:15 +0800

    11

--- src/main/java/com/anatkh/client/ClientDemo.java
+++ src/main/java/com/anatkh/client/ClientDemo.java
@@ -1,2 +1,28 @@
-package com.anatkh.client;public class ClientDemo {
+package com.anatkh.client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientDemo {
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            Socket socket = new Socket("127.0.0.1", 9999);
+            OutputStream os = socket.getOutputStream();
+            System.out.println("请输入：");
+            Scanner scanner = new Scanner(System.in);
+            String msg = scanner.nextLine();
+            os.write(msg.getBytes());
+
+            byte[] bytes = new byte[1024];
+            InputStream is = socket.getInputStream();
+            int read = is.read(bytes);
+            String s = new String(bytes, 0, read);
+            System.out.println("老板说：" + s);
+            socket.close();
+        }
+    }
 }
--- src/main/java/com/anatkh/server/ServerDemo.java
+++ src/main/java/com/anatkh/server/ServerDemo.java
@@ -1,2 +1,46 @@
-package com.anatkh.server;public class ServerDemo {
+package com.anatkh.server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ServerDemo {
+
+    public static void main(String[] args) throws IOException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        ServerSocket serverSocket = new ServerSocket(9999);
+        System.out.println("服务已启动");
+        while (true) {
+            Socket socket = serverSocket.accept();
+            System.out.println("有客户端连接");
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        handleSocket(socket);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+    }
+
+    private static void handleSocket(Socket socket) throws IOException {
+        System.out.println("线程ID："+Thread.currentThread().getId()+
+                "  线程名称："+Thread.currentThread().getName());
+        InputStream is = socket.getInputStream();
+        byte[] bytes = new byte[1024];
+        int read = is.read(bytes);
+        System.out.println("客户端："+new String(bytes,0,read));
+        OutputStream os = socket.getOutputStream();
+        os.write("没钱".getBytes());
+        socket.close();
+
+    }
 }
